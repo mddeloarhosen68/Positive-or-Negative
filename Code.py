e4a748dcfb71f774bb3fef21542addaa5cf15d38
mddeloarhosen68 <62032396+mddeloarhosen68@users.noreply.github.com>
2020-05-08 02:51:52 +0600

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,21 @@
+#Ex:1
+x = int(input("Enter your Number: "))
+
+if x > 0:
+    print("Positive Number")
+
+elif x == 0:
+    print("Zero")
+
+else:
+    print("Negative Number")
+
+#Ex:2
+x = int(input("Enter a Number: "))
+if x >= 0:
+   if x == 0:
+       print("Zero")
+   else:
+       print("Positive Number")
+else:
+   print("Negative Number")
